69d79734ff8d0fd5ddbe00f69c862d8a98016fc0
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-01 10:39:01 +0300

    HW6 done

---
+++ src/p2-homeworks/h6/HW6.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {restoreState, saveState} from './localStorage/localStorage'
+import s from './HW6.module.css'
+
+function HW6() {
+    const [value, setValue] = useState<string>('')
+
+    const save = () => {
+        saveState<string>('editable-span-value', value)
+    }
+    const restore = () => {
+        setValue(restoreState<string>('editable-span-value', value))
+    }
+
+    return (
+        <div>
+            <hr/>
+            homeworks 6
+
+            {/*should work (должно работать)*/}
+            <div className={s.editableSpanContainer}>
+                <SuperEditableSpan
+                    value={value}
+                    onChangeText={setValue}
+                    spanProps={{children: value ? undefined : 'enter text...'}}
+                />
+            </div>
+            <div className={s.buttonContainer}>
+                <SuperButton onClick={save}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
+            </div>
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeSuperEditableSpan/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW6
