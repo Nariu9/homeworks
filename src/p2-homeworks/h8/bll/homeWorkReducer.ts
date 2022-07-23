56ca233b9ef2bc146e1cc2166baa0b7e4c7fd3c1
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-23 14:52:28 +0300

    small fixes in homeWorkReducer, HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,22 +1,5 @@
 import {UserType} from "../HW8";
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            return action.payload === 'up'
-                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
-                : action.payload === 'down'
-                    ? [...state].sort((a, b) => a.name.localeCompare(b.name)).reverse()
-                    : state;
-        }
-        case 'check': {
-            return state.filter(p => p.age > action.payload)
-        }
-        default:
-            return state
-    }
-}
-
 type ActionType = sortUpDownActionType | check18ActionType
 
 type sortUpDownActionType = {
@@ -27,4 +10,21 @@ type sortUpDownActionType = {
 type check18ActionType = {
     type: 'check'
     payload: 18
+}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+    switch (action.type) {
+        case 'sort': {
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+                : action.payload === 'down'
+                    ? [...state].sort((a, b) => a.name.localeCompare(b.name)).reverse()
+                    : state;
+        }
+        case 'check': {
+            return state.filter(p => p.age >= action.payload)
+        }
+        default:
+            return state
+    }
 }
